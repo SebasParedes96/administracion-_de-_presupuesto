e6d828a088591e4df28160612f71f44b75111dad
Sebas <50669169+SebasParedes96@users.noreply.github.com>
2022-09-18 05:39:19 -0300

    fix(backend) - Add new routes and fixed routes -Modify scripts of db

---
+++ routes.js
@@ -0,0 +1,181 @@
+const router = require('express').Router()
+const conexion = require('./config/conexion')
+
+// routes
+
+//get 10 operations
+
+router.get('/:id_usser/operations', (req, res) => {
+    const { id_usser } = req.params
+    let sql = `select 
+    operations.id as id,
+    concept as concept,
+    date_format(date, "%Y-%m-%d") as date,
+    amount as amount,
+    type_operations.description as type,
+    id_category as id_category,
+    categories.description as category
+    from operations 
+    inner join type_operations on type_operations.id = operations.id_type_operation
+    inner join categories on categories.id = operations.id_category 
+    where id_usser =  ? AND leavingDate is null ORDER BY date desc limit 10`
+    conexion.query(sql, [id_usser], (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+})
+
+//get 10 operations for type
+
+router.get('/:id_usser/operations/:type', (req, res) => {
+    const { id_usser, type } = req.params
+    let sql = `select
+    operations.id as id,
+    concept as concept,
+    date as date,
+    amount as amount,
+    type_operations.description as type,
+    id_category as id_category,
+    categories.description as category
+    from operations 
+    inner join type_operations on type_operations.id = operations.id_type_operation
+    inner join categories on categories.id = operations.id_category 
+    where id_usser =  ? AND id_type_operation = ? AND leavingDate is null ORDER BY date desc limit 10`
+    conexion.query(sql, [id_usser, type], (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+})
+
+
+//get categories
+
+router.get('/categories', (req, res) => {
+    let sql = `select * from categories`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+})
+
+//get type_operations
+
+router.get('/types', (req, res) => {
+    let sql = `select * from type_operations`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+})
+
+//get balance
+
+router.get('/:id_usser', (req, res) => {
+    const { id_usser } = req.params
+    let sql = 'select sum(case when id_type_operation = 1 then amount else - amount end) as balance from operations where id_usser = ?'
+    conexion.query(sql, [id_usser], (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows)
+        }
+    })
+})
+
+//add operation
+
+router.post('/', (req, res) => {
+    const { concept, amount, date, category, type, usser } = req.body
+    let sql = `insert into operations
+    (concept, amount, date, id_category, id_type_operation, id_usser) 
+    values('${concept}','${amount}','${date}','${category}','${type}','${usser}')`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            res.json({ status: 'Added operation!' })
+        }
+    })
+})
+
+//add user
+
+router.post('/register', (req, res) => {
+    const { name,  lastName, email, password } = req.body
+    let sql = `insert into ussers(name,lastName,email,password) 
+    values('${name}','${lastName}','${email}','${password}')`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            res.json({ status: 'Added operation!' })
+        }
+    })
+})
+
+//auth
+
+router.post('/auth', (req, res) => {
+    const { email, password } = req.body
+    let sql = `select id from ussers where email = '${email}' and password ='${password}'`
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) throw err
+        else {
+            if (rows.length === 0) {
+                res.json(false)
+            } else {
+                res.json(rows)
+            }
+        }
+    })
+})
+
+// modify operation
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { concept, amount, date, id_category } = req.body
+
+    let sql = `update operations set
+    concept = '${concept}',
+    amount = '${amount}',
+    date = '${date}',
+    id_category = '${id_category}'
+    where id = '${id}'`
+
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) {
+            throw err
+        } else {
+            res.json({ status: 'Modified operation!' })
+        }
+    })
+})
+
+
+// delete operation
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    let sql = `update operations set
+    leavingDate = now()
+    where id = '${id}'`
+
+    conexion.query(sql, (err, rows, fields) => {
+        if (err) {
+            throw err
+        } else {
+            res.json({ status: 'Deleted operation!' })
+        }
+    })
+})
+
+// -------------------
+
+module.exports = router;
--- scripts.sql
+++ scripts.sql
@@ -1,33 +1,43 @@
 
---creacion tabla usuarios--
-CREATE TABLE IF NOT EXISTS `usuario` (
+--create table ussers--
+CREATE TABLE IF NOT EXISTS `ussers` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria',
-  `nombre` varchar(250) NOT NULL COMMENT 'nombre cliente',
-  `apellidos` varchar(250) NOT NULL COMMENT 'Apellidos cliente',
+  `name` varchar(250) NOT NULL COMMENT 'nombre cliente',
+  `lastName` varchar(250) NOT NULL COMMENT 'Apellidos cliente',
   `email` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `fechaAlta` date NOT NULL,
-  PRIMARY KEY (`id`)
-)
---creacion tabla operaciones--
-CREATE TABLE IF NOT EXISTS `operaciones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria',
-  `concepto` text NOT NULL COMMENT 'concepto',
-  `monto` decimal(11,2) NOT NULL,
-  `fecha` date NOT NULL,
-  `id_tipo_operacion` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `fechaAlta` date NOT NULL,
-  `fechaBaja` date,
+  `startDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
---creacion de tabla tipo operacion--
-CREATE TABLE IF NOT EXISTS `tipo_operaciones` (
+--create table operations--
+CREATE TABLE IF NOT EXISTS `operations` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria',
-  `descripcion` text NOT NULL COMMENT 'descripcion',
+  `concept` text NOT NULL COMMENT 'concepto',
+  `amount` decimal(11,2) NOT NULL,
+  `date` date NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `id_type_operation` int(11) NOT NULL,
+  `id_usser` int(11) NOT NULL,
+  `startDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `leavingDate` TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+--create table type_operations--
+CREATE TABLE IF NOT EXISTS `type_operations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria',
+  `description` text NOT NULL COMMENT 'descripcion',
   PRIMARY KEY (`id`)
 );
 
---inser datos estaticos--
-insert into tipo_operaciones (descripcion) values ('Ingreso');
-insert into tipo_operaciones (descripcion) values ('Egreso');
+-- create table categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria',
+  `description` text NOT NULL COMMENT 'descripcion',
+  PRIMARY KEY (`id`)
+);
+
+--insert static data--
+insert into type_operations (description) values ('Ingreso'),('Egreso');
+
+insert into categories (description) values ('sin categoria'),('servicio'),('compra'),('comida'),('trasporte'),('varios');
+
